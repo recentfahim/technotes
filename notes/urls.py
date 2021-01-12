40a46f7062961006922c2d3ac6b7cf3c18c7635f
recnetfahim <recentfahim13@gmail.com>
2021-01-13 00:56:09 +0600

    update and delete note

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CreateNote, ListNote
+from .views import CreateNote, ListNote, DeleteNote, EditNote
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('create/', csrf_exempt(CreateNote.as_view()), name='create_note'),
     path('list/', csrf_exempt(ListNote.as_view()), name='list_note'),
+    path('delete/<int:id>', csrf_exempt(DeleteNote.as_view()), name='delete_note'),
+    path('edit/<int:id>', csrf_exempt(EditNote.as_view()), name='edit_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -20,3 +20,33 @@ class ListNote(View):
         user = request.user
         notes = Note.objects.filter(user=user)
         return render(request, 'notes/list.html', context={'notes': notes})
+
+
+class DeleteNote(View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        note = Note.objects.filter(user=user, id=kwargs.get('id')).first()
+        note.delete()
+
+        return redirect('list_note')
+
+
+class EditNote(View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        note = Note.objects.filter(user=user, id=kwargs.get('id')).first()
+        context = {
+            'note': note
+        }
+
+        return render(request, 'notes/create.html', context=context)
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        user = request.user
+        note = Note.objects.filter(user=user, id=kwargs.get('id')).first()
+        note.title = data.get('title')
+        note.description = data.get('description')
+        note.save()
+
+        return redirect('list_note')
